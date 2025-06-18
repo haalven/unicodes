2dffe3484d13fea09d9850a1be6171bbb49fb6de
haalven <147269352+haalven@users.noreply.github.com>
2025-06-18 09:50:48 +0200

    Update unicode.py

--- unicode.py
+++ unicode.py
@@ -48,6 +48,42 @@ categories = {
     'C' : 'Other'
 }
 
+# ASCII controls (decimal)
+controls = {
+    0   : 'NUL - Null',
+    1   : 'SOH - Start of Heading',
+    2   : 'STX - Start of Text',
+    3   : 'ETX - End of Text',
+    4   : 'EOT - End of Transmission',
+    5   : 'ENQ - Enquiry',
+    6   : 'ACK - Acknowledgement',
+    7   : 'BEL - Bell',
+    8   : 'BS - Backspace',
+    9   : 'HT - Horizontal Tab',
+    10   : 'LF - Line Feed (New Line)',
+    11   : 'VT - Vertical Tab',
+    12   : 'FF - Form Feed',
+    13   : 'CR - Carriage Return',
+    14   : 'SO - Shift Out',
+    15   : 'SI - Shift In',
+    16   : 'DLE - Data Link Escape',
+    17   : 'DC1 - Device Control 1',
+    18   : 'DC2 - Device Control 2',
+    19   : 'DC3 - Device Control 3',
+    20   : 'DC4 - Device Control 4',
+    21   : 'NAK - Negative Acknowledgement',
+    22   : 'SYN - Synchronous Idle',
+    23   : 'ETB - End of Transmission Block',
+    24   : 'CAN - Cancel',
+    25   : 'EM - End of Medium',
+    26   : 'SUB - Substitute',
+    27   : 'ESC - Escape',
+    28   : 'FS - File Separator',
+    29   : 'GS - Group Separator',
+    30   : 'RS - Record Separator',
+    31   : 'US - Unit Separator'
+}
+
 
 # xterm formatting
 def f(code): return '\x1B[' + str(code) + 'm'
@@ -57,8 +93,11 @@ def c(code): return f('38;5;' + str(code))
 # common output
 def unicode_info(number, hexnum, char):
 
-    try: ucname = unicodedata.name(char)
-    except Exception: ucname = 'unknown'
+    if number < 32:
+        ucname = controls[number]
+    else:
+        try: ucname = unicodedata.name(char)
+        except Exception: ucname = 'unknown'
 
     try: category = categories[unicodedata.category(char)]
     except Exception: category = 'unknown'
@@ -84,13 +123,13 @@ def unicode_info(number, hexnum, char):
 myname = argv.pop(0).split('/')[-1]
 
 syntax = f'syntax: {myname} <type> <input>\n'
-syntax += 'types: s (string), d (decimal), h (hex) or r (range)'
+syntax += 'types: s (string), d (decimal), h (hex), b (utf8 byte seq) or r (range)'
 
 if len(argv) < 2: exit(syntax)
 
 typ, inp = argv[0].lower(), argv[1]
 
-if not typ in 'sdhr': exit(syntax)
+if not typ in 'sdhbr': exit(syntax)
 
 
 match typ:
@@ -131,6 +170,23 @@ match typ:
         hx = hex(nm)
 
         ch = chr(nm)
+
+        unicode_info(nm, hx, ch)
+
+
+    # utf8 byte sequence
+
+    case 'b':
+
+        seq = str(inp).replace(' ', '').lower()
+
+        byte_seq = bytes.fromhex(seq)
+
+        ch = byte_seq.decode('utf-8')
+
+        nm = ord(ch)
+
+        hx = hex(nm)
 
         unicode_info(nm, hx, ch)
 
